d67a004165d0db92f0df24daad1033230a1ffb15
sama-500 <gs536406@student.upm.edu.my>
2024-12-27 16:29:24 +0300

    python programs

---
+++ first.py
@@ -0,0 +1,13 @@
+print("hello welcom to the py programme")
+print(5)
+name= "lana"
+age=13
+weight=19
+print("name", name)
+print("your age is" ,age)
+
+print(type(name))
+print(type(age))
+print(type(weight))
+age=str(age)
+print(type(age))
