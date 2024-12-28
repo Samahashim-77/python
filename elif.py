6768122cd8e3eb9c746a3955e90e8d95c8dec062
sama-500 <gs536406@student.upm.edu.my>
2024-12-28 19:13:10 +0300

    ifelse

---
+++ elif.py
@@ -0,0 +1,7 @@
+n=int(input("inter your mark") )
+if n>80:
+    print("A grade")
+elif n>65:
+    print("B grade")
+else: 
+    print ("fail")    
---
+++ if.py
@@ -0,0 +1,8 @@
+n=12
+n1=37
+if n>16:
+    print ("pass")
+    print ("congradulation")
+else:
+    print("fail")    
+    
