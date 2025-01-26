1112ada120f3da1b084c6fbc29058d1beef97296
sama-500 <gs53406@student.upm.edu.my>
2025-01-26 16:39:42 +0300

    class and objects

--- turtle1.py
+++ turtle1.py
@@ -1,16 +1,32 @@
-first_number = int(input("enter first number: "))
-operation = input("enter operation type: ")
-second_number = int(input("enter second number: "))
-if operation == "+":
-    print(first_number + second_number)
-elif operation  == "-":
-    print(first_number - second_number)
-elif operation  == "/":
-    print(first_number / second_number)
-else:
-    print(first_number * second_number)
- 
 
+while True:
+  while True:
+    try :
+     first_number = float(input("enter first number: "))
+     break
+    except ValueError:
+    
+      input("invalid input. please enter vaild number")
+
+operation = input("enter operation type: ")
+second_number = float(input("enter second number: "))
+if operation == "+":
+      result first_number + second_number
+      elif operation  == "-":
+       result first_number - second_number 
+elif operation  == "/":
+     result first_number / second_number 
+else:
+     result = None
+if result != None:
+      print(result)
+
+ 
+repeat = input("do you want to perform another operation(y/n): ")
+if repeat == "n":
+ break   
+   
+print("program is exited")
 
 
 
